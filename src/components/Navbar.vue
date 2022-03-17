9684dcc3bcb5a5fd69e38d092ed7cecf850f9f4d
RuiCoutoESMAD <spookyrucohol@gmail.com>
2022-03-17 09:29:01 +0000

    files

---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+<div id="navbar">
+  <b-navbar toggleable="lg" type="dark" class="navbar">
+    <b-navbar-brand href="#">
+        <img src="../../Logo_PortoMedia_White (1).png">
+    </b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#">Link</b-nav-item>
+        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+
+        <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item href="#">EN</b-dropdown-item>
+          <b-dropdown-item href="#">ES</b-dropdown-item>
+          <b-dropdown-item href="#">RU</b-dropdown-item>
+          <b-dropdown-item href="#">FA</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+</template>
+
+<style scoped>
+    .navbar {
+        background-color: #000000;
+        opacity: 79%;
+    }
+</style>
