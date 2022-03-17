afcc83468d6b73fe3c072712bc0da292e9d3b785
RuiCoutoESMAD <spookyrucohol@gmail.com>
2022-03-17 15:07:58 +0000

    quem somos update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,9 +14,20 @@ of Letraset sheets containing<br> Lorem Ipsum passages, and more recently
 with desktop publishing software<br> like Aldus PageMaker including versions
 of Lorem Ipsum.</p>
     </div>
-  </div>
 
-  
+    <div class="quem somos">
+      <h1 class="header">MISSÃO</h1>
+      <p class="historia">Lorem Ipsum is simply dummy text of the printing and typesetting
+industry. <br> Lorem Ipsum has been the industry's standard dummy text
+ever since the<br> 1500s, when an unknown printer took a galley of type 
+and scrambled it to<br> make a type specimen book. It has survived not 
+only five centuries, but also<br> the leap into electronic typesetting, remaining
+essentially unchanged. It was<br> popularised in the 1960s with the release
+of Letraset sheets containing<br> Lorem Ipsum passages, and more recently
+with desktop publishing software<br> like Aldus PageMaker including versions
+of Lorem Ipsum.</p>
+    </div>
+  </div>
 
 </template>
 
