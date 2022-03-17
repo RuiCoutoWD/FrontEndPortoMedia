17274ee579572174382240f12d0ea6b56ba5dc01
RuiCoutoESMAD <spookyrucohol@gmail.com>
2022-03-17 16:21:28 +0000

    outdoors, contactos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,51 +1,8 @@
 <template>
   <div class="home">
     <Navbar />
-
-    <div class="quemsomos">
-      <b-container class="bv-example-row">
-        <b-row>
-          <b-col>
-            <h1 class="header">A NOSSA HISTÓRIA</h1>
-            <p class="historia">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. <br />
-              Lorem Ipsum has been the industry's standard dummy text ever since
-              the<br />
-              1500s, when an unknown printer took a galley of type and scrambled
-              it to<br />
-              make a type specimen book. It has survived not only five
-              centuries, but also<br />
-              the leap into electronic typesetting, remaining essentially
-              unchanged. It was<br />
-              popularised in the 1960s with the release of Letraset sheets
-              containing<br />
-              Lorem Ipsum passages, and more recently with desktop publishing
-              software<br />
-              like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-            <h1 class="header">MISSÃO</h1>
-            <p class="historia">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. <br />
-              Lorem Ipsum has been the industry's standard dummy text ever since
-              the<br />
-              1500s, when an unknown printer took a galley of type and scrambled
-              it to<br />
-              make a type specimen book. It has survived not only five
-              centuries, but also<br />
-              the leap into electronic typesetting, remaining essentially
-              unchanged. It was<br />
-              popularised in the 1960s with the release of Letraset sheets
-              containing<br />
-              Lorem Ipsum passages, and more recently with desktop publishing
-              software<br />
-              like Aldus PageMaker including versions of Lorem Ipsum.
-            </p></b-col>
-          <b-col>
-            <h1 class="header">PARCEIROS</h1></b-col>
-        </b-row>
-      </b-container>
+    <div>
+      <h1 class="header">OS NOSSOS OUTDOORS</h1>
     </div>
   </div>
 </template>
@@ -69,10 +26,8 @@ export default {
   color: #a58c57;
   text-decoration: none;
   text-align: left;
+  margin-left: 110px;
   margin-top: 40px;
 }
 
-.historia {
-  text-align: left;
-}
 </style>
