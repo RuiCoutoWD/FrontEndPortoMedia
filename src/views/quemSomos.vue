3ee433c629b1df005135e2eeabac43ba5d5cf1bd
RuiCoutoESMAD <spookyrucohol@gmail.com>
2022-03-17 15:10:01 +0000

    quem somos

--- src/views/quemSomos.vue
+++ src/views/quemSomos.vue
@@ -1,18 +1,61 @@
 <template>
-  <div class="quem somos">
-      <h1 class="header">LOCALIZAÇÃO</h1>
+  <div class="home">
+    <Navbar />
+
+    <div class="quem somos">
+      <h1 class="header">A NOSSA HISTÓRIA</h1>
+      <p class="historia">Lorem Ipsum is simply dummy text of the printing and typesetting
+industry. <br> Lorem Ipsum has been the industry's standard dummy text
+ever since the<br> 1500s, when an unknown printer took a galley of type 
+and scrambled it to<br> make a type specimen book. It has survived not 
+only five centuries, but also<br> the leap into electronic typesetting, remaining
+essentially unchanged. It was<br> popularised in the 1960s with the release
+of Letraset sheets containing<br> Lorem Ipsum passages, and more recently
+with desktop publishing software<br> like Aldus PageMaker including versions
+of Lorem Ipsum.</p>
+    </div>
+
+    <div class="quem somos">
+      <h1 class="header">MISSÃO</h1>
+      <p class="historia">Lorem Ipsum is simply dummy text of the printing and typesetting
+industry. <br> Lorem Ipsum has been the industry's standard dummy text
+ever since the<br> 1500s, when an unknown printer took a galley of type 
+and scrambled it to<br> make a type specimen book. It has survived not 
+only five centuries, but also<br> the leap into electronic typesetting, remaining
+essentially unchanged. It was<br> popularised in the 1960s with the release
+of Letraset sheets containing<br> Lorem Ipsum passages, and more recently
+with desktop publishing software<br> like Aldus PageMaker including versions
+of Lorem Ipsum.</p>
+    </div>
   </div>
 
 </template>
 
+<script>
+// @ is an alias to /src
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Navbar,
+  },
+};
+</script>
+
 <style scoped>
-    .header{
+    .header {
         font-family: Kayak Sans;
-        font-size: 50px;
-        margin-left: 1rem;
-        margin-top: 5rem;
+        font-size: 35px;
         color: #a58c57;
         text-decoration: none;
+        text-align: left;
+        margin-left: 50px;
+        margin-top: 40px;
+    }
+
+    .historia {
+      text-align: left;
+        margin-left: 50px;
     }
 </style>
-
