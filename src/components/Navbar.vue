e625fb5d79404deb428c5763416a04d117fc1406
Jorge Daniel <jorge.daniel11@outlook.com>
2022-03-17 12:03:22 +0000

    navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,45 +1,67 @@
 <template>
-<div id="navbar">
-  <b-navbar toggleable="lg" type="dark" class="navbar">
-    <b-navbar-brand href="#">
-        <img src="../../Logo_PortoMedia_White (1).png">
-    </b-navbar-brand>
+  <div id="navbar">
+    <b-navbar toggleable="lg" type="dark" class="navbar d-flex">
+      <b-navbar-brand href="#">
+        <img
+          src="../assets/logo_portomedia_white.png"
+          width="60px"
+          height="60px"
+          class="logo"
+        />
+      </b-navbar-brand>
 
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <a class="navBarItem p-3" href="#">Quem Somos</a>
+          <a class="navBarItem p-3" href="#">Outdoors</a>
+          <a class="navBarItem p-3" href="#">Contactos</a>  
+        </b-navbar-nav>
 
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav>
-        <b-nav-item href="#">Link</b-nav-item>
-        <b-nav-item href="#" disabled>Disabled</b-nav-item>
-      </b-navbar-nav>
-
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-
-        <b-nav-item-dropdown text="Lang" right>
-          <b-dropdown-item href="#">EN</b-dropdown-item>
-          <b-dropdown-item href="#">ES</b-dropdown-item>
-          <b-dropdown-item href="#">RU</b-dropdown-item>
-          <b-dropdown-item href="#">FA</b-dropdown-item>
-        </b-nav-item-dropdown>
-
-        <b-nav-item-dropdown right>
-          <!-- Using 'button-content' slot -->
-          <template #button-content>
-            <em>User</em>
-          </template>
-          <b-dropdown-item href="#">Profile</b-dropdown-item>
-          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-        </b-nav-item-dropdown>
-      </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
-</div>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto userImg ms-auto p-3">
+          <a href="#">
+            <img
+              src="../assets/user.png"
+              class="m-3"
+              height="30px"
+              width="30px"
+            />
+          </a>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
 </template>
 
 <style scoped>
-    .navbar {
-        background-color: #000000;
-        opacity: 79%;
-    }
+.navbar {
+  background-color: #000000;
+  opacity: 79%;
+  height: 65px;
+  margin-top: 3rem;
+}
+
+.logo {
+  padding: 0;
+  margin-left: 3rem;
+}
+
+.navBarItem {
+  font-family: Kayak Sans;
+  font-size: 25px;
+  margin-left: 1rem;
+  color: #fcfff7;
+  text-decoration: none;
+}
+
+.navBarItem:hover {
+  color: #a58c57;
+  text-decoration: none;
+}
+
+.userImg {
+  position: relative;
+  margin-right: 1rem;
+}
 </style>
