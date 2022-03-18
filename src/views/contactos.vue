987ad8d73918d2a6374546dc904def923c40e3e5
RuiCoutoESMAD <spookyrucohol@gmail.com>
2022-03-18 17:03:35 +0000

    commit

--- src/views/contactos.vue
+++ src/views/contactos.vue
@@ -1,18 +1,6 @@
 <template>
   <div class="row">
-    <div class="contactos">
-      <b-container class="bv-example-row">
-        <b-row>
-          <b-col>
-            <h1 class="header">LOCALIZAÇÃO</h1>
-            <h1 class="header">CONTACTOS</h1>
-          </b-col>
-          <b-col>
-            <h1 class="header">CONTACTE-NOS</h1>
-          </b-col>
-        </b-row>
-      </b-container>
-    </div>
+
   </div>
 </template>
 
