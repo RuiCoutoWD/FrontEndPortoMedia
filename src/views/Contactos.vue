6d34fc18baf057ca89e4c7e21272f1a278bed8cb
RuiCoutoESMAD <spookyrucohol@gmail.com>
2022-03-21 10:30:02 +0000

    contactos

--- src/views/Contactos.vue
+++ src/views/Contactos.vue
@@ -6,13 +6,25 @@
         <b-row>
           <b-col>
             <h1 class="header">LOCALIZAÇÃO</h1>
+            <div class="contactos">
+              <img src="../assets/mapa.png" class="img">
+              <p></p>
+              <a>Zona Industrial Arcos do Sardão, Avenida Arcos do Sardão,<br>
+Nº 391-A, 4430-434, Vila Nova de Gaia</a>
+            </div>
             <h1 class="header">CONTACTOS</h1>
             <div class="contactos">
-              <img src="../assets/email.png" alt="email" class="email">
+              <img src="../assets/email.png" alt="email" class="icons">
+              <a>: mail@planetapublicidade.com</a>
+              <p></p>
+              <img src="../assets/telefone.png" alt="telefone" class="icons">
+              <a>: 22 145 01 20</a>
             </div>
           </b-col>
           <b-col>
             <h1 class="header">CONTACTE-NOS</h1></b-col>
+            <input v-model="message" placeholder="edit me">
+            <p>Message is: {{ message }}</p>
         </b-row>
       </b-container>
     </div>
@@ -33,7 +45,7 @@ export default {
 
 <style scoped>
 
-.email {
+.icons {
   width: 30px;
   height: 30px;
 }
@@ -52,6 +64,11 @@ text-align: left;
 
 .historia {
   text-align: left;
+}
+
+.img{
+  width: 450px;
+  height: 280px;
 }
 </style>
 
