dff666610e8096c64ccb71805389b0d6405fa66a
RuiCoutoESMAD <spookyrucohol@gmail.com>
2022-03-21 11:51:32 +0000

    contactos

--- src/views/Contactos.vue
+++ src/views/Contactos.vue
@@ -1,30 +1,45 @@
 <template>
   <div class="home">
-
     <div class="quemsomos">
       <b-container class="bv-example-row">
         <b-row>
           <b-col>
             <h1 class="header">LOCALIZAÇÃO</h1>
             <div class="contactos">
-              <img src="../assets/mapa.png" class="img">
-              <p></p>
-              <a>Zona Industrial Arcos do Sardão, Avenida Arcos do Sardão,<br>
-Nº 391-A, 4430-434, Vila Nova de Gaia</a>
+              <img src="../assets/mapa.png" class="img" />
+              <p class="morada">
+                Zona Industrial Arcos do Sardão, Avenida Arcos do Sardão,<br />
+                Nº 391-A, 4430-434, Vila Nova de Gaia
+              </p>
             </div>
             <h1 class="header">CONTACTOS</h1>
             <div class="contactos">
-              <img src="../assets/email.png" alt="email" class="icons">
+              <img src="../assets/email.png" alt="email" class="icons" />
               <a>: mail@planetapublicidade.com</a>
               <p></p>
-              <img src="../assets/telefone.png" alt="telefone" class="icons">
+              <img src="../assets/telefone.png" alt="telefone" class="icons" />
               <a>: 22 145 01 20</a>
             </div>
           </b-col>
           <b-col>
-            <h1 class="header">CONTACTE-NOS</h1></b-col>
-            <input v-model="message" placeholder="edit me">
-            <p>Message is: {{ message }}</p>
+            <h1 class="header">CONTACTE-NOS</h1>
+            <div class="contactos">
+              <input v-model="message" class="edittext" placeholder="Nome*" />
+              <p class="space"></p>
+              <input v-model="message" class="edittext" placeholder="Email*" />
+              <p class="space"></p>
+              <input v-model="message" class="edittext" placeholder="Número" />
+              <p class="space"></p>
+              <textarea
+                v-model="message"
+                class="edittext2"
+                placeholder="Mensagem"
+              ></textarea>
+              <p></p>
+              <button class="botao">Enviar</button>
+            </div>
+            <p class="space"></p>
+          </b-col>
         </b-row>
       </b-container>
     </div>
@@ -44,14 +59,13 @@ export default {
 </script>
 
 <style scoped>
-
 .icons {
   width: 30px;
   height: 30px;
 }
 
 .contactos {
-text-align: left;
+  text-align: left;
 }
 .header {
   font-family: Kayak Sans;
@@ -66,9 +80,38 @@ text-align: left;
   text-align: left;
 }
 
-.img{
+.img {
   width: 450px;
   height: 280px;
+}
+
+.edittext {
+  width: 500px;
+  font-weight: bold;
+  font-size: 14px;
+}
+
+.space {
+  height: 10px;
+}
+
+.edittext2 {
+  width: 500px;
+  height: 120px;
+  font-weight: bold;
+  font-size: 14px;
+}
+
+.morada {
+  font-size: 15px;
+  text-decoration: none;
+}
+
+.botao {
+  background-color: #fcfff7;
+  color: #303d7a;
+  width: 100px;
+  border-width: 1px;
 }
 </style>
 
