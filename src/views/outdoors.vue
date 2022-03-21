70c4c6eab04bd4811e3043c69b0fc39ccdff2ab9
RuiCoutoESMAD <spookyrucohol@gmail.com>
2022-03-21 15:11:56 +0000

    outdoors

--- src/views/outdoors.vue
+++ src/views/outdoors.vue
@@ -1,3 +1,19 @@
+<template>
+  <div class="home">
+    <div class="quemsomos">
+      <b-container class="bv-example-row">
+        <b-row>
+          <b-col>
+            <h1 class="header">OS NOSSOS OUTDOORS</h1>
+          </b-col>
+          <b-col>
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
+  </div>
+</template>
+
 <script>
 // @ is an alias to /src
 //import Navbar from "@/components/Navbar.vue";
@@ -9,3 +25,14 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.header {
+  font-family: Kayak Sans;
+  font-size: 35px;
+  color: #a58c57;
+  text-decoration: none;
+  text-align: left;
+  margin-top: 40px;
+}
+</style>
