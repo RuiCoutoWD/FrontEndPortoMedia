3e2e28858469edc4009d3963e6efd2e5d67b4192
RuiCoutoESMAD <spookyrucohol@gmail.com>
2022-03-23 17:33:34 +0000

    admin calendar

--- src/views/admin.vue
+++ src/views/admin.vue
@@ -1,15 +1,57 @@
 <template>
   <div class="home">
-
     <div class="quemsomos">
       <b-container class="bv-example-row">
         <b-row>
           <b-col>
             <h1 class="header">Alteração de Email</h1>
             <h1 class="header">Alteração de outdoors visíveis</h1>
-            </b-col>
+          </b-col>
           <b-col>
-            <h1 class="header">Registo de alugueres</h1></b-col>
+            <h1 class="header">Registo de alugueres</h1>
+            <div>
+              <b-row>
+                <b-col>
+                  <div class="contactos">
+                    <input
+                      v-model="message"
+                      class="edittext"
+                      placeholder="Nome de Cliente"
+                    />
+                    <p class="space"></p>
+                    <input
+                      v-model="message"
+                      class="edittext"
+                      placeholder="Email"
+                    />
+                    <p class="space"></p>
+                    <input
+                      v-model="message"
+                      class="edittext"
+                      placeholder="Face Alugada"
+                    />
+                    <p class="space"></p>
+                    <input
+                      v-model="message"
+                      class="edittext"
+                      placeholder="Preço do aluguer"
+                    />
+                  </div>
+                </b-col>
+                <b-col>
+                  <div class="calendar">
+                    <b-calendar
+                    width="200px"
+                      v-model="value"
+                      @context="onContext"
+                      locale="pt-PT"
+                      hide-header="true"
+                    ></b-calendar>
+                  </div>
+                </b-col>
+              </b-row>
+            </div>
+          </b-col>
         </b-row>
       </b-container>
     </div>
@@ -29,7 +71,6 @@ export default {
 </script>
 
 <style scoped>
-
 .header {
   font-family: Kayak Sans;
   font-size: 25px;
@@ -39,13 +80,11 @@ export default {
   margin-top: 9rem;
 }
 
-.header2 {
-  font-family: Kayak Sans;
-  font-size: 35px;
-  color: #a58c57;
-  text-decoration: none;
-  text-align: left;
-  margin-top: 2rem;
+.calendar {
+  margin-right: 6rem;
 }
 
+.contactos {
+  text-align: left;
+}
 </style>
