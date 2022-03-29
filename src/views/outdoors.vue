2f1d9b49f0bdb7ce5cb5854c64ae4f60c84986a1
RuiCoutoESMAD <spookyrucohol@gmail.com>
2022-03-29 14:10:33 +0100

    changes

--- src/views/outdoors.vue
+++ src/views/outdoors.vue
@@ -6,14 +6,11 @@
           <b-col>
             <h1 class="header">OS NOSSOS OUTDOORS</h1>
             <div @click="showModal()" class="div">
-              <b-card
-                style="max-width: 20rem"
-                class="mb-2 border-0"
-              >
-              <img src="../assets/Face1.png" class="face">
+              <b-card style="max-width: 20rem" class="mb-2 border-0">
+                <img src="../assets/Face1.png" class="face" />
               </b-card>
             </div>
-                        <div @click="showModal()" class="div">
+            <div @click="showModal()" class="div">
               <b-card
                 img-alt="Image"
                 img-height="200px"
@@ -22,12 +19,11 @@
                 style="max-width: 20rem"
                 class="mb-2 border-0"
               >
-              <img src="../assets/Face4.png" class="face">
+                <img src="../assets/Face4.png" class="face" />
               </b-card>
             </div>
-                        <div @click="showModal()" class="div">
+            <div @click="showModal()" class="div">
               <b-card
-                
                 img-alt="Image"
                 img-height="200px"
                 img-top
@@ -35,12 +31,11 @@
                 style="max-width: 20rem"
                 class="mb-2 border-0"
               >
-              <img src="../assets/Face7.png" class="face">
+                <img src="../assets/Face7.png" class="face" />
               </b-card>
             </div>
-                        <div @click="showModal()" class="div">
+            <div @click="showModal()" class="div">
               <b-card
-                
                 img-alt="Image"
                 img-height="200px"
                 img-top
@@ -48,12 +43,11 @@
                 style="max-width: 20rem"
                 class="mb-2 border-0"
               >
-              <img src="../assets/Face10.png" class="face">
+                <img src="../assets/Face10.png" class="face" />
               </b-card>
             </div>
-                        <div @click="showModal()" class="div">
+            <div @click="showModal()" class="div">
               <b-card
-                
                 img-alt="Image"
                 img-height="200px"
                 img-top
@@ -61,13 +55,12 @@
                 style="max-width: 20rem"
                 class="mb-2 border-0"
               >
-              <img src="../assets/Face13.png" class="face">
+                <img src="../assets/Face13.png" class="face" />
               </b-card>
             </div>
-
           </b-col>
           <b-col class="space">
-                        <div @click="showModal()" class="div">
+            <div @click="showModal()" class="div">
               <b-card
                 img-alt="Image"
                 img-height="200px"
@@ -76,25 +69,11 @@
                 style="max-width: 20rem"
                 class="mb-2 border-0"
               >
-              <img src="../assets/Face2.png" class="face">
-              </b-card>
-                        </div> 
-                                    <div @click="showModal()" class="div">
-              <b-card
-                
-                img-alt="Image"
-                img-height="200px"
-                img-top
-                tag="article"
-                style="max-width: 20rem"
-                class="mb-2 border-0"
-              >
-              <img src="../assets/Face5.png" class="face">
+                <img src="../assets/Face2.png" class="face" />
               </b-card>
             </div>
-                        <div @click="showModal()" class="div">
+            <div @click="showModal()" class="div">
               <b-card
-                
                 img-alt="Image"
                 img-height="200px"
                 img-top
@@ -102,12 +81,11 @@
                 style="max-width: 20rem"
                 class="mb-2 border-0"
               >
-              <img src="../assets/Face8.png" class="face">
+                <img src="../assets/Face5.png" class="face" />
               </b-card>
             </div>
-                        <div @click="showModal()" class="div">
+            <div @click="showModal()" class="div">
               <b-card
-                
                 img-alt="Image"
                 img-height="200px"
                 img-top
@@ -115,12 +93,11 @@
                 style="max-width: 20rem"
                 class="mb-2 border-0"
               >
-              <img src="../assets/Face11.png" class="face">
+                <img src="../assets/Face8.png" class="face" />
               </b-card>
             </div>
-                        <div @click="showModal()" class="div">
+            <div @click="showModal()" class="div">
               <b-card
-                
                 img-alt="Image"
                 img-height="200px"
                 img-top
@@ -128,15 +105,11 @@
                 style="max-width: 20rem"
                 class="mb-2 border-0"
               >
-              <img src="../assets/Face14.png" class="face">
+                <img src="../assets/Face11.png" class="face" />
               </b-card>
             </div>
-
-             </b-col>
-          <b-col class="space"> 
-                        <div @click="showModal()" class="div">
+            <div @click="showModal()" class="div">
               <b-card
-                
                 img-alt="Image"
                 img-height="200px"
                 img-top
@@ -144,25 +117,13 @@
                 style="max-width: 20rem"
                 class="mb-2 border-0"
               >
-              <img src="../assets/Face3.png" class="face">
-              </b-card>
-                        </div>
-                                    <div @click="showModal()" class="div">
-              <b-card
-                
-                img-alt="Image"
-                img-height="200px"
-                img-top
-                tag="article"
-                style="max-width: 20rem"
-                class="mb-2 border-0"
-              >
-              <img src="../assets/Face6.png" class="face">
+                <img src="../assets/Face14.png" class="face" />
               </b-card>
             </div>
-                        <div @click="showModal()" class="div">
+          </b-col>
+          <b-col class="space">
+            <div @click="showModal()" class="div">
               <b-card
-               
                 img-alt="Image"
                 img-height="200px"
                 img-top
@@ -170,12 +131,11 @@
                 style="max-width: 20rem"
                 class="mb-2 border-0"
               >
-              <img src="../assets/Face9.png" class="face">
+                <img src="../assets/Face3.png" class="face" />
               </b-card>
             </div>
-                        <div @click="showModal()" class="div">
+            <div @click="showModal()" class="div">
               <b-card
-                
                 img-alt="Image"
                 img-height="200px"
                 img-top
@@ -183,12 +143,11 @@
                 style="max-width: 20rem"
                 class="mb-2 border-0"
               >
-              <img src="../assets/Face12.png" class="face">
+                <img src="../assets/Face6.png" class="face" />
               </b-card>
             </div>
-                        <div @click="showModal()" class="div">
+            <div @click="showModal()" class="div">
               <b-card
-                
                 img-alt="Image"
                 img-height="200px"
                 img-top
@@ -196,51 +155,97 @@
                 style="max-width: 20rem"
                 class="mb-2 border-0"
               >
-              <img src="../assets/Face15.png" class="face">
+                <img src="../assets/Face9.png" class="face" />
+              </b-card>
+            </div>
+            <div @click="showModal()" class="div">
+              <b-card
+                img-alt="Image"
+                img-height="200px"
+                img-top
+                tag="article"
+                style="max-width: 20rem"
+                class="mb-2 border-0"
+              >
+                <img src="../assets/Face12.png" class="face" />
+              </b-card>
+            </div>
+            <div @click="showModal()" class="div">
+              <b-card
+                img-alt="Image"
+                img-height="200px"
+                img-top
+                tag="article"
+                style="max-width: 20rem"
+                class="mb-2 border-0"
+              >
+                <img src="../assets/Face15.png" class="face" />
               </b-card>
             </div>
           </b-col>
         </b-row>
-              <b-modal
-              size="lg"
-                ref="my-modal"
-                hide-footer
-                centered
-                class="modal"
-              >
-              <template #modal-header>
-                <span class="nome">Face 01</span>
-              </template>
-              <img src="../assets/Face1.png" width="766px" overlay>
-                <p></p>
-                <button @click="hideModal"  class="confirmar">Pedir Orçamento</button>
-              </b-modal>
+        <b-modal size="lg" ref="my-modal" hide-footer centered class="modal">
+          <template #modal-header>
+            <span class="nome">Face 01</span>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+              @click="hideModal3()"
+            >
+              X
+            </button>
+          </template>
+          <img src="../assets/Face1.png" width="766px" overlay />
+          <p></p>
+          <div class="buttons">
+            <b-row>
+              <b-col>
+                <img src="../assets/exportar.png" class="export" />
+              </b-col>
+              <b-col>
+                <button class="favorito">Adicionar aos Favoritos</button>
+              </b-col>
+              <b-col>
+                <button @click="hideModal" class="confirmar">
+                  Pedir Orçamento
+                </button>
+              </b-col>
+            </b-row>
+          </div>
+        </b-modal>
 
-              <b-modal
-                ref="my-modal2"
-                hide-footer
-                centered
-                class="modal"
-              >
-              <template #modal-header>
-                <span class="nome">Pedir Orçamento</span>
-              </template>
-              <div class="contactos">
-              <input v-model="message" class="edittext" placeholder="Nome*" />
-              <p></p>
-              <input v-model="message" class="edittext" placeholder="Email*" />
-              <p></p>
-              <input v-model="message" class="edittext" placeholder="Contacto" />
-              <p></p>
-              <textarea
-                v-model="message"
-                class="edittext"
-                placeholder="Mensagem"
-              ></textarea>
-              <p></p>
-              <button class="confirmar2" @click="pedidoAlert()">Enviar</button>
-            </div>
-              </b-modal>
+        <b-modal ref="my-modal2" hide-footer centered class="modal">
+          <template #modal-header>
+            <span class="nome">Pedir Orçamento</span>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+              @click="hideModal2()"
+              
+            >
+              X
+            </button>
+          </template>
+          <div class="contactos">
+            <input v-model="message" class="edittext" placeholder="Nome*" />
+            <p></p>
+            <input v-model="message" class="edittext" placeholder="Email*" />
+            <p></p>
+            <input v-model="message" class="edittext" placeholder="Contacto" />
+            <p></p>
+            <textarea
+              v-model="message"
+              class="edittext"
+              placeholder="Mensagem"
+            ></textarea>
+            <p></p>
+            <button class="confirmar2" @click="pedidoAlert()">Enviar</button>
+          </div>
+        </b-modal>
       </b-container>
     </div>
   </div>
@@ -263,6 +268,9 @@ export default {
       this.$refs["my-modal"].hide();
       this.$refs["my-modal2"].show();
     },
+    hideModal3() {
+      this.$refs["my-modal"].hide();
+    },
     hideModal2() {
       this.$refs["my-modal2"].hide();
     },
@@ -283,7 +291,23 @@ export default {
 </script>
 
 <style scoped>
-.div:hover{
+.contactos {
+  text-align: right;
+}
+
+.close {
+  font-size: 20px;
+  border-width: 0px;
+  background-color: #ffffff;
+  align-content: space-between;
+  color: #e80b0b;
+}
+
+.export {
+  width: 30px;
+}
+
+.div:hover {
   cursor: pointer;
 }
 
@@ -294,17 +318,15 @@ export default {
 }
 
 .edittext {
-  width: 464px;
+  width: 100%;
   font-weight: bold;
   font-size: 14px;
 }
 
-.face{
+.face {
   width: 400px;
   height: 280px;
 }
-
-
 
 .space {
   margin-top: 12rem;
@@ -326,27 +348,44 @@ export default {
 .confirmar {
   background-color: #fcfff7;
   color: #e80b0b;
-  margin-left: 36.6rem;
   width: 180px;
   border-radius: 6px;
   border-color: #e80b0b;
+  border-width: 1px;
+}
+
+.favorito {
+  background-color: #fcfff7;
+  color: #a58c57;
+  margin-left: 15.6rem;
+  width: 200px;
+  border-radius: 6px;
+  border-color: #a58c57;
+  border-width: 1px;
+}
+
+.favorito:hover {
+  color: #fcfff7;
+  background-color: #a58c57;
+  width: 200px;
+  border-radius: 6px;
+  border-color: #a58c57;
   border-width: 1px;
 }
 
 .confirmar2 {
   background-color: #fcfff7;
   color: #e80b0b;
-  margin-left: 17.7rem;
-  width: 180px;
+  width: 140px;
   border-radius: 6px;
   border-color: #e80b0b;
   border-width: 1px;
+  align-content: flex-end;
 }
 
 .confirmar:hover {
   color: #fcfff7;
   background-color: #e80b0b;
-  margin-left: 36.6rem;
   width: 180px;
   border-radius: 6px;
 }
@@ -354,7 +393,6 @@ export default {
 .confirmar2:hover {
   color: #fcfff7;
   background-color: #e80b0b;
-  width: 180px;
   border-radius: 6px;
 }
 </style>
