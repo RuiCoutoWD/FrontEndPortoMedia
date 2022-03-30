e262a9ba83728995a2e29c357cb9b5bdf7bc4932
RuiCoutoESMAD <spookyrucohol@gmail.com>
2022-03-30 15:24:33 +0100

    comentários home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,11 @@
 <template>
-  <!-- <video-background src="../assets/city.mp4" style="max-height: 400px; height: 100vh"> -->
-  <!-- </video-background> -->
-
-
+<!-- Secção do vídeo -->
   <video autoplay muted loop id="myVideo">
   <source src="../assets/city3.mp4" type="video/mp4">
 </video>
 </template>
 
 <script>
-// @ is an alias to /src
-// import VideoBackground from "vue-responsive-video-background-player";
-
 export default {
   name: "Home",
   components: {
@@ -21,6 +15,7 @@ export default {
 </script>
 
 <style scoped>
+/* Estilização de vídeo */
 #myVideo {
   position: fixed;
   right: 0;
